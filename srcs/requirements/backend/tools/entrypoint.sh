fc8e1cf3f4a246d7d720266a5b8354785e5916a5
Hotaru <jeremyhurpy@gmail.com>
2025-04-03 03:44:29 +0200

    remove env file in backend and frontend, and add dockerignore

--- srcs/requirements/backend/tools/entrypoint.sh
+++ srcs/requirements/backend/tools/entrypoint.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
-# ENV_SECRET="/run/secrets/backend_env_file"
-# ENV_TARGET="/usr/src/app/.env"
+ENV_SECRET="/run/secrets/backend_env_file"
+ENV_TARGET="/usr/src/app/.env"
 
-# echo "🔍 Checking for Docker secret at $ENV_SECRET..."
-# if [ -f "$ENV_SECRET" ]; then
-#   echo "✅ Found Docker secret, copying to $ENV_TARGET"
-#   cp "$ENV_SECRET" "$ENV_TARGET"
-# else
-#   echo "⚠️ No secret found at $ENV_SECRET. Proceeding without .env injection."
-# fi
+echo "🔍 Checking for Docker secret at $ENV_SECRET..."
+if [ -f "$ENV_SECRET" ]; then
+  echo "✅ Found Docker secret, copying to $ENV_TARGET"
+  cp "$ENV_SECRET" "$ENV_TARGET"
+else
+  echo "⚠️ No secret found at $ENV_SECRET. Proceeding without .env injection."
+fi
 
 echo "Waiting for PostgreSQL to start..."
 while ! nc -z postgres 5432; do
